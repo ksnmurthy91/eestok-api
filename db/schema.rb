952d41510ac77516c9e0b8fecdbe412869b0dc00
Satya Narayana Murthy Kota <satyakota91@gmail.com>
2021-02-19 14:45:17 +0530

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_075708) do
+ActiveRecord::Schema.define(version: 2021_02_19_082015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "description", null: false
@@ -30,5 +41,6 @@ ActiveRecord::Schema.define(version: 2021_02_19_075708) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
 end
